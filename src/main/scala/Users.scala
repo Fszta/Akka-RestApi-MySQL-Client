1f5313ba03f92d33c25dc6faf6d5551ff6471045
rcd <antoinefer@hotmail.com>
2020-01-30 20:06:35 +0100

    Add method list of case class to json

--- src/main/scala/Users.scala
+++ src/main/scala/Users.scala
@@ -1,3 +1,16 @@
+import spray.json.DefaultJsonProtocol._
+import spray.json._
+
 trait Users {
   case class User(id: Int, firstname: String, lastname: String, email: String)
+  /**
+    * Convert a list of User class to JsonString format
+    * @param
+    * @return
+    */
+  def userListToJson(users: List[User]): String = {
+    implicit val userJsonFormat = jsonFormat4(User)
+    val jsonString = users.toJson.toString
+    jsonString
+  }
 }
